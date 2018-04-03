5d5087ce1f75a30c776bccaa24f452c78977c815
Luis Malta <luiso.malta@gmail.com>
2018-04-03 18:27:02 -0300

    Reformulação do metodo de Jacobi

--- Sistemas lineares/Metodos Iterativos/gauss_jacobi.c
+++ Sistemas lineares/Metodos Iterativos/gauss_jacobi.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-w
+
 
 
 int main(){
   int k = 0, coluna, linha, pare = 0;
-  double e, somatorio1, somatorio2;
+  double e, somatorio;
 
   //Leitura da matriz
   printf("Entre com o tamnho da matriz e a precisao:\n");
   scanf("%d %d %lf", &linha, &coluna, &e);
-  double x[coluna], x_inicial[coluna], matriz[linha][coluna], erro[coluna-1];
+  double x_anterior[coluna -1], x_atual[coluna -1], matriz[linha][coluna];
 
   printf("Entre com a matriz\n");
   for(int i = 0; i < linha; i++){
@@ -19,47 +19,45 @@ int main(){
       scanf("%lf", &matriz[i][j]);
     }
   }
-
   //Leitura da estimativa inicial
   printf("Entre com a estimativa inicial.\n");
-  for(int i = 0; i < coluna; i++){
-      scanf("%lf", x_inicial[i]);
-      printf("........");
-      x[i] = x_inicial[i];
-      printf("i: %d", i);
+  for(int i = 0; i < coluna -1; i++){
+      scanf("%lf", x_anterior[i]);
   }
 
   //Iterações
   while(1){
-    printf("........");
-    somatorio1 = 0;
-    somatorio2 = 0;
-
-    for(int i = 0; i < coluna-1; i++){
-        x_inicial[i] = x[i];
+    printf("Entrou.\n");
+    for(int i = 0; i <= linha; i++){
+      somatorio = 0;
+      for(int j = 0; j < coluna -1; j++){
+        if(i != j){
+          somatorio -= matriz[i][j] * x_anterior[j];
+        }
+      }
+      x_atual[i] = (matriz[i][coluna] - somatorio)/matriz[i][i];
     }
 
-    for(int i = 0; i < coluna-1; i++){
-      for(int j = 1; j < i ; i++){
-        somatorio1 += -matriz[i][j] * x[j];
-      }
-
-      for(int j = i + 1; j <= coluna -1; j++){
-        somatorio2 = -matriz[i][j] * x[j];
-      }
-
-      x[i] = (somatorio1 + somatorio2) / (matriz[i][i] + (x[coluna] / matriz[i][i]));
-    }
-
-    for(int i = 0; i < coluna -1; i++){
-      if((fabs(x[i] - x_inicial[i]) / fabs(x[i])) > e){
+    pare = 0;
+    for(int i = 0; i < coluna - 1; i++){
+      if(((fabs(x_atual[i] - x_anterior[i]))/fabs(x_atual[i])) > e){
         pare = 1;
       }
     }
+
     if(pare == 0){
       break;
-    } else {
-      k++;
+    }
+
+    for(int i = 0; i < coluna; i++){
+      x_anterior[i] = x_atual[i];
+      matriz[i][0] = x_atual[0];
+    }
+
+    for(int i = 0; i < linha; i++){
+      for(int j = 0; j < coluna - 1; j++){
+        matriz[i][j] = x_atual[j];
+      }
     }
 
   }
